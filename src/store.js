19ac452248bf93ef4d52dd8b9efc921863b33b55
i320626 <biplob.ghosh@sap.com>
2020-07-28 21:29:09 +0530

    initial commit
    Change-Id: I6532e9271ded0adf9adda0a7ba9b739d939cab77

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { createStore, combineReducers} from 'redux';
+import { todos } from "./todos/reducers.js";
+
+const reducers = {
+    todos, 
+};
+
+const rootReducer = combineReducers(reducers);
+
+export const configureStore = () =>  createStore(rootReducer);
